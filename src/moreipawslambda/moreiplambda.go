c54eb0fc6c156695730b68c46401cdc4ebc087ef
Bret Edwards <sliverman69@gmail.com>
2019-11-03 00:32:07 -0700

    moreiplambda.go: initial build of moreip for golang on AWS Lambda

---
+++ src/moreipawslambda/moreiplambda.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+//Handler is needed to be called by lambda
+func handleRequest(ctx context.Context, req events.ALBTargetGroupRequest) (events.ALBTargetGroupResponse, error) {
+	responseBody := req.Headers["x-forwarded-for"]
+	response := events.ALBTargetGroupResponse{Body: responseBody, StatusCode: 200, IsBase64Encoded: false, StatusDescription: "200 OK", Headers: map[string]string{}}
+	response.Headers["Content-Type"] = "text/html; charset=utf-8"
+	fmt.Printf("Request: %v\n", req)
+	fmt.Printf("RemoteAddr: %v\n", responseBody)
+	fmt.Printf("response: %v\n", response)
+	return response, nil
+}
+
+func main() {
+	fmt.Printf("Starting Handler\n")
+	lambda.Start(handleRequest)
+}
