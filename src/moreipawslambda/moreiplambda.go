0caa10d7957ac1a34980b65cf7ef843a1837cd41
Bret Edwards <sliverman69@gmail.com>
2019-11-28 10:44:53 -0800

    moreiplambda.go: pretty print request JSON to log

--- src/moreipawslambda/moreiplambda.go
+++ src/moreipawslambda/moreiplambda.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -13,7 +14,11 @@ func handleRequest(ctx context.Context, req events.ALBTargetGroupRequest) (event
 	responseBody := req.Headers["x-forwarded-for"] + "\n"
 	response := events.ALBTargetGroupResponse{Body: responseBody, StatusCode: 200, IsBase64Encoded: false, StatusDescription: "200 OK", Headers: map[string]string{}}
 	response.Headers["Content-Type"] = "text/html; charset=utf-8"
-	fmt.Printf("Request: %v\n", req)
+	prettyJSON, err := json.MarshalIndent(req, "", "  ")
+	if err != nil {
+		fmt.Printf("Could not unmarshal json. Error:\n%v", err)
+	}
+	fmt.Printf("Request:\n%s\n", string(prettyJSON))
 	fmt.Printf("RemoteAddr: %v\n", responseBody)
 	fmt.Printf("response: %v\n", response)
 	return response, nil
